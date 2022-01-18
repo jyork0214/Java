cad7e06a6e83f7f8ac4cf9a4e5552e8835719856
James York <jyork0214@gmail.com>
2022-01-18 13:29:41 -0500

    Updated Java content

--- Client.java
+++ Client.java
@@ -1,4 +1,14 @@
+/*
+-----------------------------------------------
+Program Name:  Client.java
+Author:  James York
+Date:  18JAN22@1327
+Purpose:  A basic construct.
 
+Notes.
+    None.
+-----------------------------------------------
+*/
 public class Client
 {
     public static void main(String[] args)
