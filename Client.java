693bae2c4b04b12b4ae8e2de097a1e79f1ecfee8
James York <jyork0214@gmail.com>
2022-01-18 13:36:20 -0500

    Updated Java content

--- Client.java
+++ Client.java
@@ -1,9 +1,9 @@
 /*
 -----------------------------------------------
 Program Name:  Client.java
-Author:  James York
-Date:  18JAN22@1327
-Purpose:  A basic construct.
+      Author:  James York
+        Date:  18JAN22@1327
+     Purpose:  A basic construct.
 
 Notes.
     None.
